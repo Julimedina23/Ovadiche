e9d5a69321f61752addc4e8933ea9edca90d11fe
Julimedina23 <162385488+Julimedina23@users.noreply.github.com>
2024-03-10 10:33:31 -0500

    hola

---
+++ javascript/fundamentos.js
@@ -0,0 +1,16 @@
+// Este es un comentario de JavaScript de una sola linea
+/*
+este es un 
+comentario de 
+varias lineas
+*/
+
+
+alert("hola")
+
+"use strict"
+let miVariable = "este es un mensaje de prueba"
+console.log(miVariable)
+
+
+
