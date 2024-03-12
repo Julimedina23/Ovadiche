b392ddbcfefc40db77f1e99876cca1d29ad3ddb8
Julimedina23 <162385488+Julimedina23@users.noreply.github.com>
2024-03-12 17:41:30 -0500

    hola

---
+++ javascript/desafios.js
@@ -0,0 +1,8 @@
+"use strict"
+// hacer un programa que salude 
+/*comsole.log("hola")
+document.write("hola")*/
+
+// hacer programa que salude por nombre, si no recibe nombre solo saludar
+const nombreUsuario = prompt("ingrese su nombre")
+console.log(nombreUsuario)
