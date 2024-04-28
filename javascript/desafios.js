a3cab1189ddc3472f150d747a7d270dc47f1588b
Julimedina23 <162385488+Julimedina23@users.noreply.github.com>
2024-04-27 22:50:21 -0500

    desafio

--- javascript/desafios.js
+++ javascript/desafios.js
@@ -6,3 +6,10 @@ document.write("hola")*/
 // hacer programa que salude por nombre, si no recibe nombre solo saludar
 /* const nombreUsuario = prompt("ingrese su nombre")
 console.log(nombreUsuario) */
+"use strict"
+// Hacer un programa  que pida al usuarui un numero entero y muestre los valores desde uno hasta el mumero ingresado.
+const numero = parseInt(prompt(`ingrese un numero entero`));
+for ( let i = 1; <= numero; i++) {
+    console.log('valor en i: ', i)
+    document.write(i);
+}
